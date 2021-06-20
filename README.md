a1364e4fdd71c3ef19220c573a863460f3687c2f
MoltenCoreDev <craxell.tv@gmail.com>
2021-06-20 22:00:40 +0200

    Final touches

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-Welcome to ssovim!
+# Welcome to ssovim
 
 S stolen
 S shitty
@@ -7,11 +7,14 @@ VIM
 
 This is a basic config to make nvim your IDE!
 
-Dependencies:
-- paq-nvim - A plugin manager, it will handle everything else. Just follow the installation guides on: https://github.com/savq/paq-nvim.
+## Dependencies
+
+- neovim nightly (0.5+)
+- paq-nvim - A plugin manager, it will handle everything else. [here](https://github.com/savq/paq-nvim.)
 - Nodejs, for coc.nvim to work.
 
+## Installation
 
-Planned features:
-- better tab support
-- better custom keybinds w/ easy config
+```bash
+git clone https://github.com/MoltenCoreDev/ssovim ~/.config/nvim
+```
--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,20 +1,43 @@
 -- plugins.lua
 
-vim.g.coc_global_extensions = {"coc-json", "coc-tsserver", "coc-pairs", "coc-lua", "coc-pyright"}
+vim.g.coc_global_extensions = {"coc-json", "coc-tsserver", "coc-pairs", "coc-lua", "coc-pyright", "coc-discord-rpc"}
 
-require 'paq-nvim' {
-    'savq/paq-nvim';
-	'neoclide/coc.nvim';
-	'preservim/nerdtree';
-    'glepnir/dashboard-nvim';
-    'nvim-telescope/telescope.nvim';
-    'nvim-lua/plenary.nvim';
-    'nvim-lua/popup.nvim';
-    'sharkdp/bat';
-    'sharkdp/fd';
-    'BurntSushi/ripgrep';
-    'vim-airline/vim-airline';
-    'vim-airline/vim-airline-themes';
-    'nvim-treesitter/nvim-treesitter';
-    'morhetz/gruvbox';
+require "paq-nvim" {
+    "savq/paq-nvim";
+	"neoclide/coc.nvim";
+	"preservim/nerdtree";
+    "glepnir/dashboard-nvim";
+    "nvim-telescope/telescope.nvim";
+    "nvim-lua/plenary.nvim";
+    "nvim-lua/popup.nvim";
+    "sharkdp/bat";
+    "sharkdp/fd";
+    "BurntSushi/ripgrep";
+    "vim-airline/vim-airline";
+    "vim-airline/vim-airline-themes";
+    "nvim-treesitter/nvim-treesitter";
+    "morhetz/gruvbox";
 }
+
+-- Dashboard.nvim
+
+footers = {
+    "Doofenshmirtz Evil inc. 2021©",
+    "MoltenCoreDev 2021©",
+    "Also try Megalomacs©!",
+    "Also try LunarVim©!",
+    "Also try going outside!"
+}
+
+
+vim.g.dashboard_custom_header = {
+    "        Welcome to SSOVim!",
+    "The stolen shitty opinionated vim."
+}
+
+math.randomseed(os.clock())
+vim.g.dashboard_custom_footer = {footers[math.floor(math.random(#(footers)))]}
+vim.g.dashboard_custom_section = {
+    a = {description = {"Find a file."}, command = "DashboardFindFile"},
+}
+vim.g.dashboard_default_executive = "telescope"
